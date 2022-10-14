04d79d933795f8a6d711f5eeb73965cc12bcb5e0
Kunal <mailto.guptakunal@gmail.com>
2022-10-14 19:31:10 +0530

    done

--- block-BNaacu/routes/students.js
+++ block-BNaacu/routes/students.js
@@ -1,18 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-//book routes
-
 router.get('/new', (req, res) => {
   res.render('studentForm');
 });
 
 router.post('/', (req, res) => {
   res.send(req.body);
-  Student.create(req.body, (err, student) => {
-    console.log(err, student);
-  });
-  res.send(`${req.body.name} added successfully`);
 });
 
 router.get('/', (req, res) => {
@@ -20,24 +14,9 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-  var id = req.params.id;
-  Student.findById(id, (err, students) => {
-    console.log(err, students);
-    var name = { name: 'kunal', email: 'kunal@gmail.com' };
-    res.render('studentDetails', { name: name });
+  res.render('studentDetail', {
+    student: { name: 'rahul', email: 'rahul@altcampus.io' },
   });
 });
 
-router.get('/:id/edit', (req, res) => {
-  //find the book from db
-  // render a update form with book data
-});
-
-router.put('/:id', (req, res) => {
-  //findbyidandupdate
-});
-
-router.delete('/:id', (req, res) => {
-  //delete using id
-});
 module.exports = router;
